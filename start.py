e7143c2a983acb259bb2f5fbf127399872e93072
Paul <grom.gom@mail.ru>
2021-07-04 10:19:53 +0300

    try/except

--- start.py
+++ start.py
@@ -11,49 +11,49 @@ def main(argv):
     -c - Output current fistname and lastname.
     -a - Output all operations.
     """
-
-    if argv[1] == "-l":
-
-        firstname = input("Input firstname: ")
-        lastname = input("Input lastname: ")
-        if firstname and lastname:
-            user = User(firstname, lastname)
-            user.name_length()
-        else:
-            print("firstname or lastname is empty")
-
-    elif argv[1] == "-c":
-
-        firstname = input("Input firstname: ")
-        lastname = input("Input lastname: ")
-        if firstname and lastname:
-            user = User(firstname, lastname)
-            user.current_firstname_and_lastname()
-        else:
-            print("firstname or lastname is empty")
+    try:
+        if argv[1] == "-l":
     
-    elif argv[1] == "-a":
+            firstname = input("Input firstname: ")
+            lastname = input("Input lastname: ")
+            if firstname and lastname:
+                user = User(firstname, lastname)
+                user.name_length()
+            else:
+                print("firstname or lastname is empty")
+        elif argv[1] == "-c":
+            firstname = input("Input firstname: ")
+            lastname = input("Input lastname: ")
+            if firstname and lastname:
+                user = User(firstname, lastname)
+                user.current_firstname_and_lastname()
+            else:
+                    print("firstname or lastname is empty")
         
-        firstname = input("Input firstname: ")
-        lastname = input("Input lastname: ")
-        if firstname and lastname:
-            user = User(firstname, lastname)
-            filesys = FileSystem("user.txt")
-            db = DataBase()
-            user.name_length()
-            user.past_name(filesys.read_file())
-            user.current_firstname_and_lastname()
-            db.check_directory()
-            filesys.check_file()
-            filesys.create_file(user.validate())
-        else:
-            print("firstname or lastname is empty")
+        elif argv[1] == "-a":     
+            firstname = input("Input firstname: ")
+            lastname = input("Input lastname: ")
+            if firstname and lastname:
+                user = User(firstname, lastname)
+                filesys = FileSystem("user.txt")
+                db = DataBase()
+                user.name_length()
+                user.past_name(filesys.read_file())
+                user.current_firstname_and_lastname()
+                db.check_directory()
+                filesys.check_file()
+                filesys.create_file(user.validate())
+            else:
+                print("firstname or lastname is empty")
 
-    elif argv[1] == "-h":
-        print(help_args)
-    else:
+        elif argv[1] == "-h":
+            print(help_args)
+        else:
+            print(help_args)
+    except IndexError:
         print(help_args)
 
 if __name__ == '__main__':
     main(argv)
+
     
