7cd3d0bea628e4e6c088f421c990f3d189a31061
Pavel Grebenshikov <65436450+PavelGrebenshikov@users.noreply.github.com>
2021-09-13 23:20:14 +0300

    refactoring

--- start.py
+++ start.py
@@ -1,57 +1,51 @@
 from classes.database import DataBase
 from classes.user import User
 from classes.filesystem import FileSystem
-from sys import argv
+import argparse
 
-def main(argv):
-    
-    help_args = """
-    -l - Output length you'r firstname and lastname.
-    -h - Output help arguments.
-    -c - Output current fistname and lastname.
-    -a - Output all operations.
-    """
-    try:
-        if argv[1] == "-l":
-    
-            firstname = input("Input firstname: ")
-            lastname = input("Input lastname: ")
-            if firstname and lastname:
-                user = User(firstname, lastname)
-                user.name_length()
-            else:
-                print("firstname or lastname is empty")
-        elif argv[1] == "-c":
-            firstname = input("Input firstname: ")
-            lastname = input("Input lastname: ")
-            if firstname and lastname:
-                user = User(firstname, lastname)
-                user.current_firstname_and_lastname()
-            else:
-                    print("firstname or lastname is empty")
-        
-        elif argv[1] == "-a":     
-            firstname = input("Input firstname: ")
-            lastname = input("Input lastname: ")
-            if firstname and lastname:
-                user = User(firstname, lastname)
-                filesys = FileSystem("user.txt")
-                db = DataBase()
-                user.name_length()
-                user.past_name(filesys.read_file())
-                user.current_firstname_and_lastname()
-                db.check_directory()
-                filesys.check_file()
-                filesys.create_file(user.validate())
-            else:
-                print("firstname or lastname is empty")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--lenght", help="Output length you'r firstname and lastname.", action="store_true")
+    parser.add_argument("-c", "--current", help="Output current fistname and lastname", action="store_true")
+    parser.add_argument("-a", "--all", help="Output all operations", action="store_true")
+    args = parser.parse_args()
 
-        elif argv[1] == "-h":
-            print(help_args)
+    if args.lenght:
+        firstname = input("Input firstname: ")
+        lastname = input("Input lastname: ")
+
+        if firstname and lastname:
+            user = User(firstname, lastname)
+            user.name_length()
         else:
-            print(help_args)
-    except IndexError:
-        print(help_args)
+            print("firstname or lastname is empty")
+    elif args.current:
+        firstname = input("Input firstname: ")
+        lastname = input("Input lastname: ")
+
+        if firstname and lastname:
+            user = User(firstname, lastname)
+            user.current_firstname_and_lastname()
+        else:
+            print("firstname or lastname is empty")
+    elif args.all:
+        firstname = input("Input firstname: ")
+        lastname = input("Input lastname: ")
+        if firstname and lastname:
+            user = User(firstname, lastname)
+            filesys = FileSystem("user.txt")
+            db = DataBase()
+            user.name_length()
+            user.past_name(filesys.read_file())
+            user.current_firstname_and_lastname()
+            db.check_directory()
+            filesys.check_file()
+            filesys.create_file(user.validate())
+        else:
+            print("firstname or lastname is empty")
+    else:
+        print("Show options -h or --help")
+    
 
 if __name__ == '__main__':
     main(argv)
